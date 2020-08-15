b861f4b9704bde01e3a8bf73be9a83f18aea7f36
Germán Fajardo Sánchez <gerfagerfa@gmail.com>
2020-08-15 16:38:43 -0300

    finished

--- lib/book_detail.dart
+++ lib/book_detail.dart
@@ -69,73 +69,184 @@ class BookDetail extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
 
-                  Text(
-                    book.title,
-                    style: GoogleFonts.catamaran(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      height: 1,
-                    ),
-                  ),
-
-                  Text(
-                    book.author.fullname,
-                    style: GoogleFonts.catamaran(
-                      fontSize: 18,
-                      color: Colors.grey,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 8),
-                    child: Row(
-                      children: [
-
-                        Row(
-                          children: <Widget>[
-
-                            Icon(Icons.star, size: 20, color: kStarsColor,),
-                            Icon(Icons.star, size: 20, color: kStarsColor,),
-                            Icon(Icons.star, size: 20, color: kStarsColor,),
-                            Icon(Icons.star, size: 20, color: kStarsColor,),
-                            Icon(Icons.star_half, size: 20, color: kStarsColor,),
-
-                          ],
-                        ),
-
-                        SizedBox(
-                          width: 12,
-                        ),
-
-                        Text(
-                          book.score,
-                          style: GoogleFonts.catamaran(
-                            fontSize: 16,
-                            color: Colors.grey,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-
-                      ],
-                    ),
-                  ),
-
                   Expanded(
-                    child: SingleChildScrollView(
-                      physics: BouncingScrollPhysics(),
-                      child: Text(
-                        book.description,
-                        style: GoogleFonts.catamaran(
-                          fontSize: 16,
-                        ),
+                    child: Container(
+                      padding: EdgeInsets.only(right: 32, left: 32, bottom: 16,),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+
+                          Text(
+                            book.title,
+                            style: GoogleFonts.catamaran(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              height: 1,
+                            ),
+                          ),
+
+                          Text(
+                            book.author.fullname,
+                            style: GoogleFonts.catamaran(
+                              fontSize: 18,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8),
+                            child: Row(
+                              children: [
+
+                                Row(
+                                  children: <Widget>[
+
+                                    Icon(Icons.star, size: 20, color: kStarsColor,),
+                                    Icon(Icons.star, size: 20, color: kStarsColor,),
+                                    Icon(Icons.star, size: 20, color: kStarsColor,),
+                                    Icon(Icons.star, size: 20, color: kStarsColor,),
+                                    Icon(Icons.star_half, size: 20, color: kStarsColor,),
+
+                                  ],
+                                ),
+
+                                SizedBox(
+                                  width: 12,
+                                ),
+
+                                Text(
+                                  book.score,
+                                  style: GoogleFonts.catamaran(
+                                    fontSize: 16,
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                          ),
+
+                          Expanded(
+                            child: SingleChildScrollView(
+                              physics: BouncingScrollPhysics(),
+                              child: Text(
+                                book.description,
+                                style: GoogleFonts.catamaran(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ),
+
+                        ],
                       ),
                     ),
                   ),
 
                   Container(
                     height: 100,
-                    color: Colors.white,
+                    width: size.width,
+                    padding: EdgeInsets.only(top: 16, left: 32, right: 32, bottom: 32,),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+
+                        Container(
+                          width: size.width / 2 - 44,
+                          decoration: BoxDecoration(
+                            color: kPrimaryColor,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: kPrimaryColor.withOpacity(0.4),
+                                spreadRadius: 2,
+                                blurRadius: 2,
+                                offset: Offset(0, 0),
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Want to read",
+                                  style: GoogleFonts.catamaran(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Icon(
+                                  Icons.keyboard_arrow_down,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        Container(
+                          width: size.width / 2 - 44,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                            border: Border.all(
+                              color: Colors.grey,
+                              width: 1,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.4),
+                                spreadRadius: 2,
+                                blurRadius: 2,
+                                offset: Offset(0, 0),
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Get a copy",
+                                  style: GoogleFonts.catamaran(
+                                    fontSize: 18,
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Icon(
+                                  Icons.content_copy,
+                                  color: kPrimaryColor,
+                                  size: 20,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                      ],
+                    ),
                   ),
 
                 ],
